f4f3369e92a6fd342835b41522163e489a249503
EulalieCoevoet <eulalie.coevoet@gmail.com>
2025-08-05 19:08:29 +0200

    adds animation (sphere or cube)

--- validation.py
+++ validation.py
@@ -1,4 +1,32 @@
 
+from modules.targets import Targets
+import Sofa
+
+
+class TargetController(Sofa.Core.Controller):
+
+    def __init__(self, target, effector, assembly):
+        Sofa.Core.Controller.__init__(self)
+        self.name="TargetController"
+
+        self.positions = target.getMechanicalState().position.value
+        self.steps = len(self.positions)
+        self.effector = effector
+        self.assembly = assembly
+
+        self.animationSteps = 20 # Number of steps before going to the next target
+        self.animationStep = self.animationSteps
+
+    def onAnimateBeginEvent(self, _):
+
+        if self.assembly.done:
+            self.animationStep -= 1
+            if self.steps >= 0 and self.animationStep == 0:
+                self.steps -= 1
+                self.animationStep = self.animationSteps
+                self.effector.effectorGoal = [list(self.positions[self.steps]) + [0, 0, 0, 1]]
+
+
 def createScene(rootnode):
     from utils.header import addHeader, addSolvers
     from parts.controllers.assemblycontroller import AssemblyController
@@ -26,7 +54,8 @@ def createScene(rootnode):
 
     simulation.addChild(emio)
     emio.attachCenterPartToLegs()
-    emio.addObject(AssemblyController(emio))
+    assembly = AssemblyController(emio)
+    emio.addObject(assembly)
 
     # Effector
     emio.effector.addObject("MechanicalObject", template="Rigid3", position=[0, 0, 0, 0, 0, 0, 1])
@@ -45,5 +74,15 @@ def createScene(rootnode):
 
     # Components for the connection to the real robot 
     emio.addConnectionComponents()
+
+    # cubePositions = Targets(ratio=0.1, center=[0,-150,0], size=50).cube()
+    # cube = modelling.addChild("CubeTargets")
+    # cube.addObject("MechanicalObject", position=cubePositions, showObject=True, showObjectScale=10, drawMode=0)
+
+    spherePositions = Targets(ratio=0.1, center=[0,-150,0], size=50).sphere()
+    sphere = modelling.addChild("SphereTargets")
+    sphere.addObject("MechanicalObject", position=spherePositions, showObject=True, showObjectScale=10, drawMode=0)
+
+    rootnode.addObject(TargetController(sphere, emio.effector.EffectorCoord, assembly))
 
     return rootnode
